7df3b1d9214c8ef7dfc5aa7452aa693e7271b96b
Kapil Gupta <kapilguptansit@gmail.com>
2022-02-23 09:20:36 +0530

    Added the parsing logic

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer(function (req, res) {
+	// Get the URL and parse it
+	var parsedUrl = url.parse(req.url, true);
+
+	// Get the path
+	var path = parsedUrl.pathname;
+	var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+	// Send the response
 	res.end('Hello World');
+
+	// Log the request path
+	console.log('Request Received on path:' + trimmedPath);
 });
 
+// Start the server, and have it listen on the port 3000
 server.listen(3000, function () {
 	console.log('The server is listening to port 3000 now');
 });
